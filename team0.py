244251970cf3fce34f5f1b5b2eaca41bec390b95
MrFrogIsMe <ribk5003@gmail.com>
2025-05-06 00:36:39 +0800

    chore: Rename run.py to team0.py

--- team0.py
+++ team0.py
@@ -16,14 +16,19 @@ if __name__ == "__main__":
             m, n = int(lines[1]), int(lines[2])
     print(task, m, n)
     match(task):
+        # Task 1: Find most Queens
         case 1:
             pass
+        # Task 2: Find most Bishops
         case 2:
             pass
+        # Task 3: Find most Knights
         case 3:
             pass
+        # Task 4: Find most Bishops and Knights
         case 4:
             pass
+        # Task 5: Find most Bishops and Knights with a given Queens
         case 5:
             pass
         case _:
