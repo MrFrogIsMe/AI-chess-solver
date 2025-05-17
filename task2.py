3a22bd9dfccde528e13f443cbb9b5f4fea3a2eca
annieyii <annannann0710@gmail.com>
2025-05-18 01:55:53 +0800

    if board is not square, bishops cant put it like that

--- task2.py
+++ task2.py
@@ -1,9 +1,12 @@
 def findmostBishops(m, n):
-    if m < n : m, n = n, m
     board = [[0]*n for _ in range(m)]
-    for col in range(n): board[0][col] = 1
-    for col in range(1, n-1): board[m-1][col] = 1
+    if m <= n: 
+        board[0] = [1] * n
+    else:
+        for row in range(m): board[row][0] = 1
+    if m == n:
+        for col in range(1, n-1): board[m-1][col] = 1
     for row in range(m):
         print(board[row])
     return board
-# findmostBishops(4, 3)
+findmostBishops(4, 5)
