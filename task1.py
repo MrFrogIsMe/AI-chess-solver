840bed938971b32a21bb2603ab3a8bdb43c82fbe
MrFrogIsMe <ribk5003@gmail.com>
2025-05-19 14:53:39 +0800

    refactor: Optimize is_safe function using set

--- task1.py
+++ task1.py
@@ -1,29 +1,33 @@
-def is_safe(board, row, col, m, n):
-    for i in range(m):
-        if board[i][col] == 1: return False
-    for i in range(n):
-        if board[row][i] == 1: return False
-    i, j = row - 1, col - 1
-    while i >= 0 and j >= 0:
-        if board[i][j] == 1: return False
-        i -= 1
-        j -= 1
-    i, j = row - 1, col + 1
-    while i >= 0 and j < n:
-        if board[i][j] == 1: return False
-        i -= 1
-        j += 1
+# attacked[0]: row
+# attacked[1]: col
+# attacked[2]: main diagonal
+# attacked[3]: anti diagonal
+attacked = [set() for _ in range(4)]
+
+def is_safe(row, col):
+    if row in attacked[0]: return False         # check row
+    if col in attacked[1]: return False         # check col
+    if row + col in attacked[2]: return False   # check main diagonal
+    if row - col in attacked[3]: return False   # check anti diagonal
     return True
 
 def dfs(board, row, m, n):
     if row == min(m, n):
         return board
     for col in range(n):
-        if is_safe(board, row, col, m, n):
+        if is_safe(row, col):
             board[row][col] = 1
+            attacked[0].add(row)
+            attacked[1].add(col)
+            attacked[2].add(row + col)
+            attacked[3].add(row - col)
             result = dfs(board, row+1, m, n)
             if result: return result
             board[row][col] = 0
+            attacked[0].remove(row)
+            attacked[1].remove(col)
+            attacked[2].remove(row + col)
+            attacked[3].remove(row - col)
     return None
 
 def findmostQueens(m, n):
