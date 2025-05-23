beaf78c8001506c05595371dd4f5f3537bcdd60d
annieyii <annannann0710@gmail.com>
2025-05-23 14:39:04 +0800

    task2

--- task2.py
+++ task2.py
@@ -1,9 +1,16 @@
 def findmostBishops(m, n):
-    if m < n : m, n = n, m
     board = [[0]*n for _ in range(m)]
-    for col in range(n): board[0][col] = 1
-    for col in range(1, n-1): board[m-1][col] = 1
-    for row in range(n):
-        print(board[row])
+    if m <= n:
+        for i in range (0, n, m):
+            for j in range (m):
+                board[j][i] = 1
+    else:
+        for i in range (0, m, n):
+            for j in range (n):
+                board[i][j] = 1
+    
+    if m == n:
+        for i in range (1, n-1): board[i][m-1] = 1
+    # for i in range (m): print(board[i])
     return board
-# findmostBishops(4, 4)
+# findmostBishops(6, 6)
