2a085c41553edcdf46fa527dab2f34cb3bd6e3b5
annieyii <annannann0710@gmail.com>
2025-06-03 22:43:35 +0800

    fix: Update dfs function to use index and by_row parameter for improved clarity

--- task1.py
+++ task1.py
@@ -11,17 +11,19 @@ def is_safe(row, col):
     if row - col in attacked[3]: return False   # check anti diagonal
     return True
 
-def dfs(board, row, m, n):
-    if row == min(m, n):
-        return board
-    for col in range(n):
+def dfs(board, idx, m, n, by_row):
+    limit = m if by_row else n
+    if idx == limit: return board
+    for i in range(n if by_row else m):
+        row = idx if by_row else i
+        col = i if by_row else idx
         if is_safe(row, col):
             board[row][col] = 1
             attacked[0].add(row)
             attacked[1].add(col)
             attacked[2].add(row + col)
             attacked[3].add(row - col)
-            result = dfs(board, row+1, m, n)
+            result = dfs(board, idx+1, m, n, by_row)
             if result: return result
             board[row][col] = 0
             attacked[0].remove(row)
@@ -32,5 +34,9 @@ def dfs(board, row, m, n):
 
 def findmostQueens(m, n):
     board = [[0]*n for _ in range(m)]
-    result = dfs(board, 0, m, n)
+    by_row = m <= n
+    result = dfs(board, 0, m, n, by_row)
+    for row in board:
+        print(row)
     return result
+findmostQueens(8, 3)
