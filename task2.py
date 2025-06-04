c1f8906e28cdb92f8faba79e9b4adb8579a44f85
MrFrogIsMe <ribk5003@gmail.com>
2025-06-04 19:57:34 +0800

    feat: implement task selection and input handling in team1.py

--- task2.py
+++ task2.py
@@ -1,15 +1,2 @@
 def findmostBishops(m, n):
-    board = [[0]*n for _ in range(m)]
-    if m <= n:
-        for i in range (0, n, m):
-            for j in range (m):
-                board[j][i] = 1
-    else:
-        for i in range (0, m, n):
-            for j in range (n):
-                board[i][j] = 1
-    
-    if m == n:
-        for i in range (1, n-1): board[i][m-1] = 1
-    # for i in range (m): print(board[i])
-    return board
+    pass
