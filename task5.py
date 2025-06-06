33c460ec45bf5866ac2bf10a835964fd7b9c5d79
annieyii <annannann0710@gmail.com>
2025-06-06 14:15:54 +0800

    refactor: Update knight and bishop representation in observation module; improve clarity and consistency in board state management

--- task5.py
+++ task5.py
@@ -4,9 +4,10 @@ from task5_hill_climbing import find_most_bishops_and_knights_with_queens_hill_c
 from task5_simulated_annealing import find_most_bishops_and_knights_with_queens_simulated_annealing
 def find_most_bishops_and_knights_with_queens(m, n, QueensPos):
     board, count_bishops, count_knights = find_most_bishops_and_knights_with_queens_observation(m, n, QueensPos)
-    board, count_bishops, count_knights = find_most_bishops_and_knights_with_queens_hill_climbing(m, n, QueensPos)
-    board, count_bishops, count_knights = find_most_bishops_and_knights_with_queens_simulated_annealing(m, n, QueensPos)
+    # board, count_bishops, count_knights = find_most_bishops_and_knights_with_queens_hill_climbing(m, n, QueensPos)
+    # board, count_bishops, count_knights = find_most_bishops_and_knights_with_queens_simulated_annealing(m, n, QueensPos)
     for i in range(m): print(board[i])
     print("number of bishops:", count_bishops)
     print("number of knights:", count_knights)
     return board
+find_most_bishops_and_knights_with_queens(9, 10, [(0, 0), (2, 3), (8, 6)])
--- task5_observation.py
+++ task5_observation.py
@@ -1,25 +1,26 @@
-KnightsMove = [(2, 1), (2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2), (-2, 1), (-2, -1)]
+bishop_moves = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+knight_moves = [(-1, -2), (-1, 2), (1, -2), (1, 2), (-2, -1), (-2, 1), (2, -1), (2, 1)]
 
 def ClearKnights(board, m, n, row, col):
-    for dr, dc in KnightsMove:
+    for dr, dc in knight_moves:
         nr, nc = row + dr, col + dc 
-        if 0 <= nr < m and 0 <= nc < n and board[nr][nc] == 0: board[nr][nc] = -1
+        if 0 <= nr < m and 0 <= nc < n and board[nr][nc] == '.': board[nr][nc] = 'X'
 def is_safe_knights(board, m, n,row, col):
-    for dr, dc in KnightsMove:
+    for dr, dc in knight_moves:
         nr, nc = row + dr, col + dc 
-        if 0 <= nr < m and 0 <= nc < n and board[nr][nc] > 0: return False
+        if 0 <= nr < m and 0 <= nc < n and board[nr][nc] != '.' and board[nr][nc] != 'X': return False
     return True
 def is_safe_bishops(board, m, n, row, col):
     for d in range(1, max(m, n)):
         for dr, dc in [(-d, -d), (-d, d), (d, -d), (d, d)]:
             nr, nc = row + dr, col + dc
-            if 0 <= nr < m and 0 <= nc < n and board[nr][nc] > 0: return False
+            if 0 <= nr < m and 0 <= nc < n and board[nr][nc] != '.' and board[nr][nc] != 'X': return False
     return True
 def placeKnights(board, m, n, ismainDiagonal):
     for i in range(m):
         for j in range(i % 2 if ismainDiagonal else (i + 1) % 2, n, 2):
-            if board[i][j] == 0 and is_safe_knights(board, m, n, i, j): 
-                board[i][j] = 2
+            if board[i][j] == '.' and is_safe_knights(board, m, n, i, j): 
+                board[i][j] = 'K'
                 ClearKnights(board, m, n, i, j)
     return board
 
@@ -28,14 +29,14 @@ def placeBishops(board, m, n):
     minNKnightsonLine = float('inf')
     for i in range(m):
         for j in range(n):
-            if board[i][j] != 1 and board[i][j] != -1:
+            if board[i][j] != 'Q' and board[i][j] != 'X':
                 # check whether there is knights on bishop's line
                 NKnightsonLine = 0
                 for d in range(1, max(m, n)):
                     for dr, dc in [(-d, -d), (-d, d), (d, -d), (d, d)]:
                         r, c = i + dr, j + dc
-                        if 0 <= r < m and 0 <= c < n and (board[r][c] == 2 or board[r][c] == 3): NKnightsonLine += 1
-                if NKnightsonLine == 0: board[i][j] = 3
+                        if 0 <= r < m and 0 <= c < n and (board[r][c] == 'K' or board[r][c] == 'B'): NKnightsonLine += 1
+                if NKnightsonLine == '.': board[i][j] = 3
                 if NKnightsonLine < minNKnightsonLine:
                     minNKnightsonLine = NKnightsonLine
                     nr, nc = i, j
@@ -44,39 +45,38 @@ def placeBishops(board, m, n):
     # bishops can put without erasing knights
     if minNKnightsonLine == 0: return board
     # place bishops with replacing knights
-    board[nr][nc] = 3
+    board[nr][nc] = 'B'
     return board
 def find_most_bishops_and_knights_with_queens_observation(m, n, QueensPos):
-    # queens = 1, Knights = 2, Bishops = 3, can't put -1
-    board = [[0] * n for _ in range(m)]
+    board = [['.'] * n for _ in range(m)]
     # place queens
     for qc, qr in QueensPos:
         if qr < 0 or qr >= m or qc < 0 or qc >= n:
             print("Invalid Queen position")
             return
-        board[qr][qc] = 1
+        board[qr][qc] = 'Q'
         for i in range(m): 
-            if board[i][qc] == 0: board[i][qc] = -1
+            if board[i][qc] == '.': board[i][qc] = 'X'
         for i in range(n): 
-            if board[qr][i] == 0:board[qr][i] = -1
+            if board[qr][i] == '.':board[qr][i] = 'X'
         for d in range(1, max(m, n)):
             for dr, dc in [(-d, -d), (-d, d), (d, -d), (d, d)]:
                 r, c = qr + dr, qc + dc
-                if 0 <= r < m and 0 <= c < n: board[r][c] = -1
+                if 0 <= r < m and 0 <= c < n: board[r][c] = 'X'
     # determine which diagonal can put more knights
     NmainDiagonal = NantiDiagonal = 0
     for i in range(m):
         for j in range(n):
-            if (i + j)%2 == 0 and board[i][j] == 0:
+            if (i + j)%2 == '.' and board[i][j] == '.':
                 if is_safe_knights(board, m, n, i, j) : NmainDiagonal += 1
-            elif (i - j)%2 == 1 and board[i][j] == 0: 
+            elif (i - j)%2 == '.' and board[i][j] == '.':  
                 if is_safe_knights(board, m, n, i, j): NantiDiagonal += 1
     # place knights
     board = placeKnights(board, m, n, NmainDiagonal >= NantiDiagonal)
     # place bishops
     board = placeBishops(board, m, n)
 
-    count_bishops = sum(row.count(3) for row in board)
-    count_knights = sum(row.count(2) for row in board)
+    count_bishops = sum(row.count('B') for row in board)
+    count_knights = sum(row.count('K') for row in board)
     # for i in range(m): print(board[i])
     return board, count_bishops, count_knights
