2463127c94d010f087b24d599e9448e6eb6b248c
MrFrogIsMe <ribk5003@gmail.com>
2025-06-08 17:45:11 +0800

    chore: Remove unused function findMostBishopsAndKnights

--- task4.py
+++ task4.py
@@ -1,4 +1,4 @@
 from task4_hill_climbing import find_most_bishops_and_knights_hill_climbing
+
 def findMostBishopsAndKnights(m, n):
-    board = find_most_bishops_and_knights_hill_climbing(m, n)
-    return board
+    return find_most_bishops_and_knights_hill_climbing(m, n)
